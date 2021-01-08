fa7cf49280bee061419b66b5f271ea2ed2972edb
Derek Vincent <derek.vincent@kemikal.net>
2021-01-08 13:21:29 -0500

    Added changes for the Vpn Connection to handle ipsec details.

--- customer-gateway/variables.tf
+++ customer-gateway/variables.tf
@@ -66,3 +66,88 @@ variable "transit_gateway_route_table_id" {
   type        = string
   default     = ""
 }
+
+
+variable "tunnel1_ike_versions" {
+  description = "IKE Version [ikev1, ikev2]"
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel1_phase1_dh_group_numbers" {
+  description = "Phase 1 DH group numbers [ 2 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 ]."
+  type        = list(number)
+  default     = null
+}
+
+variable "tunnel1_phase1_encryption_algorithms" {
+  description = "Phase 1 encryption algorithms [ ES128 | AES256 | AES128-GCM-16 | AES256-GCM-16 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel1_phase1_integrity_algorithms" {
+  description = "Phase 1 integrity algorithms [ SHA1 | SHA2-256 | SHA2-384 | SHA2-512 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel1_phase2_dh_group_numbers" {
+  description = "Phase 2 DH group numbers [ 2 | 5 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 ]."
+  type        = list(number)
+  default     = null
+}
+
+variable "tunnel1_phase2_encryption_algorithms" {
+  description = "Phase 2 encryption algorithms [ AES128 | AES256 | AES128-GCM-16 | AES256-GCM-16 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel1_phase2_integrity_algorithms" {
+  description = "Phase 2 integrity algorithms [ SHA1 | SHA2-256 | SHA2-384 | SHA2-512 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel2_ike_versions" {
+  description = "IKE Version [ikev1, ikev2]"
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel2_phase1_dh_group_numbers" {
+  description = "Phase 1 DH group numbers [ 2 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 ]."
+  type        = list(number)
+  default     = null
+}
+
+variable "tunnel2_phase1_encryption_algorithms" {
+  description = "Phase 1 encryption algorithms [ ES128 | AES256 | AES128-GCM-16 | AES256-GCM-16 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel2_phase1_integrity_algorithms" {
+  description = "Phase 1 integrity algorithms [ SHA1 | SHA2-256 | SHA2-384 | SHA2-512 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel2_phase2_dh_group_numbers" {
+  description = "Phase 2 DH group numbers [ 2 | 5 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 ]."
+  type        = list(number)
+  default     = null
+}
+
+variable "tunnel2_phase2_encryption_algorithms" {
+  description = "Phase 2 encryption algorithms [ AES128 | AES256 | AES128-GCM-16 | AES256-GCM-16 ]."
+  type        = list(string)
+  default     = null
+}
+
+variable "tunnel2_phase2_integrity_algorithms" {
+  description = "Phase 2 integrity algorithms [ SHA1 | SHA2-256 | SHA2-384 | SHA2-512 ]."
+  type        = list(string)
+  default     = null
+}

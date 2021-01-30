93c5760f642e0ad8965021a78904042b81f4e92b
Derek Vincent <derek.vincent@kemikal.net>
2021-01-30 17:32:44 -0500

    File Gateway NFS Share: Inital check in.

---
+++ file-gateway-nfs-share/main.tf
@@ -0,0 +1,40 @@
+locals {
+
+  name          = var.sname != "" ? lower(join("-", ["file-gateway", var.sname])) : "nfs-share"
+  instance_name = lower(join("-", [var.namespace, var.name, var.environment, local.name]))
+
+  notification_policy = var.notification_time == 0 ? jsonencode({ "Upload" : { "SettlingTimeInSeconds" : var.notification_time } }) : "{}"
+
+  ## TAGS
+  tag_name_env         = join(":", [var.namespace, "environment"])
+  tag_name_customer    = join(":", [var.namespace, "customer"])
+  tag_name_application = join(":", [var.namespace, "application"])
+
+  common_tags = map(
+    local.tag_name_env, var.environment,
+    local.tag_name_customer, var.customer,
+    local.tag_name_application, var.name,
+  )
+}
+
+resource "aws_storagegateway_nfs_file_share" "shares" {
+  client_list             = var.client_list
+  gateway_arn             = var.gateway_arn
+  location_arn            = var.location_arn
+  role_arn                = var.role_arn
+  file_share_name         = var.file_share_name
+  default_storage_class   = var.default_storage_class
+  guess_mime_type_enabled = true
+  notification_policy     = local.notification_policy
+
+  nfs_file_share_defaults {
+    directory_mode = var.directory_mode
+    file_mode      = var.file_mode
+    group_id       = var.group_id
+    owner_id       = var.owner_id
+  }
+
+  cache_attributes {
+    cache_stale_timeout_in_seconds = var.cache_ttl
+  }
+}
---
+++ file-gateway-nfs-share/variables.tf
@@ -0,0 +1,100 @@
+variable "namespace" {
+  description = "Namespace - used in tagging and name generation."
+  type        = string
+  default     = ""
+}
+
+variable "environment" {
+  description = "Environment - used in tagging and name generation."
+  type        = string
+  default     = ""
+}
+
+variable "name" {
+  description = "Name or application - used in tagging and name generation"
+  type        = string
+}
+
+variable "customer" {
+  description = "Customer (internal/external) Name - billing tag"
+  type        = string
+}
+
+variable "sname" {
+  description = "Specific name used in the nameing/tagging to distiguish multiple object of the same type."
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "gateway_arn" {
+  description = "The Storage Gateway arn to create the file share against."
+  type        = string
+}
+
+variable "client_list" {
+  description = "The client CIDRs allowed to access the Storage Gateway."
+  type        = list(string)
+}
+
+variable "location_arn" {
+  description = "The arn of the source bucket."
+  type        = string
+}
+
+variable "role_arn" {
+  description = "The rle arn that the storage gateway uses to access the bucket."
+  type        = string
+}
+
+variable "file_share_name" {
+  description = "The name of the file to be created. Required if using a bucket prefix."
+  type        = string
+  default     = null
+}
+
+variable "cache_ttl" {
+  description = "Cache time-to-live on the share when the cache is refreshed."
+  type        = string
+  default     = "600"
+}
+
+variable "default_storage_class" {
+  description = "The storage class for object placed in the bucket via the file gateway. Options: S3_STANDARD, S3_STANDARD_IA, and S3_ONEZONE_IA."
+  type        = string
+  default     = "S3_STANDARD"
+}
+
+variable "notification_time" {
+  description = "The time in seconds that an upload event will be sent on a change. A 0 disables the notification."
+  type        = number
+  default     = 0
+}
+
+variable "directory_mode" {
+  description = "The unix directory mode."
+  type        = string
+  default     = "0777"
+}
+
+variable "file_mode" {
+  description = "The unix file mode."
+  type        = string
+  default     = "0666"
+}
+
+variable "group_id" {
+  description = "The default unix group id for the file share."
+  type        = string
+  default     = "65534"
+}
+
+variable "owner_id" {
+  description = "The default unix owner id for the file share."
+  type        = string
+  default     = "65534"
+}
